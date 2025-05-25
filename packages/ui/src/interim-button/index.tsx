b2a51dfcccf47b0fdaa282685bd4091e0a9b666b
sudosubin <sudosubin@gmail.com>
2025-05-26 01:39:19 +0900

    [Y26W2-7] feat(ui): 임시 디자인 시스템 구현
    commit-id:23c3476d

---
+++ packages/ui/src/interim-button/index.tsx
@@ -0,0 +1,22 @@
+import type { ButtonHTMLAttributes, PropsWithChildren } from "react";
+
+export type InterimButtonProps = PropsWithChildren<
+  {
+    className?: string;
+  } & ButtonHTMLAttributes<HTMLButtonElement>
+>;
+
+export const InterimButton = ({
+  children,
+  className = "",
+  ...props
+}: InterimButtonProps) => {
+  return (
+    <button
+      className={`rounded-lg border border-transparent bg-neutral-900 px-4 py-1.5 font-inherit font-medium text-base text-neutral-100 transition-colors duration-200 hover:border-indigo-400 focus:outline-2 focus:outline-auto focus:outline-blue-400 dark:bg-neutral-100 dark:text-neutral-900 ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};

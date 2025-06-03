2b8394b3a9f4aac22ab33313f83dbbd3e3417d4f
sudosubin <sudosubin@gmail.com>
2025-06-04 02:28:16 +0900

    [Y26W2-13] feat(web): Sentry 설정 추가
    commit-id:50d79f7f

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,28 @@
+import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const plugins = [
+  (config: NextConfig) =>
+    withSentryConfig(config, {
+      org: "yapp-26th-web-2nd",
+      project: "26th-web-team-2-fe-web",
+      bundleSizeOptimizations: {
+        excludeDebugStatements: true,
+        excludeReplayShadowDom: true,
+        excludeReplayIframe: true,
+        excludeReplayWorker: true,
+      },
+      disableLogger: true,
+      silent: !process.env.CI,
+      sourcemaps: {
+        deleteSourcemapsAfterUpload: true,
+      },
+      telemetry: false,
+      tunnelRoute: "/monitoring",
+      widenClientFileUpload: true,
+    }),
+];
 
-export default nextConfig;
+export default plugins.reduce((acc, plugin) => plugin(acc), {
+  /* config options here */
+} satisfies NextConfig);

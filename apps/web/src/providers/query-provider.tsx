4dc5ae06b99c865aa96ef8d2c012a60beb7ee95d
goeun kim <rlarhdms6828@naver.com>
2025-06-07 02:43:45 +0900

    [Y26W2-16] fix(root): tanstack query dev tool 프로덕션에서  빌드 제외

--- apps/web/src/providers/query-provider.tsx
+++ apps/web/src/providers/query-provider.tsx
@@ -11,7 +11,9 @@ function QueryProvider({ children }: PropsWithChildren) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
+      {process.env.NODE_ENV === "development" && (
+        <ReactQueryDevtools initialIsOpen={false} />
+      )}
     </QueryClientProvider>
   );
 }

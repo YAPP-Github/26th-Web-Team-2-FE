bff4ac21fbf2597356c29d975188845525103881
goeun kim <rlarhdms6828@naver.com>
2025-06-14 21:47:00 +0900

    [Y26W2-18] fix(ui): stroybook main.ts 절대경로 대신 패키지 이름으로 대체

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -5,20 +5,20 @@ import type { StorybookConfig } from "@storybook/react-vite";
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value: string): any {
+function _getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
   stories: ["../src/*.mdx", "../src/*/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    getAbsolutePath("@storybook/addon-onboarding"),
-    getAbsolutePath("@chromatic-com/storybook"),
-    getAbsolutePath("@storybook/addon-docs"),
-    getAbsolutePath("@storybook/addon-a11y"),
-    getAbsolutePath("@storybook/addon-vitest"),
+    "@storybook/addon-onboarding",
+    "@chromatic-com/storybook",
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y",
+    "@storybook/addon-vitest",
   ],
   framework: {
-    name: getAbsolutePath("@storybook/react-vite"),
+    name: "@storybook/react-vite",
     options: {},
   },
 

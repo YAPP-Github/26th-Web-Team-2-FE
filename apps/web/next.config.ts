f86fe7d781f28d9c45f9fb15439f129b11f97769
sudosubin <sudosubin@gmail.com>
2025-06-18 00:46:32 +0900

    [Y26W2-33] refactor(web): next-plugin-svgr을 사용하도록 리팩토링

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,58 +1,31 @@
 import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
+import withSvgr from "next-plugin-svgr";
 
 const plugins = [
-  (config: NextConfig): NextConfig => ({
-    ...config,
-    webpack(config, options) {
-      const nextConfig =
-        typeof config.webpack === "function"
-          ? config.webpack(config, options)
-          : config;
-
-      nextConfig.module ||= {};
-      nextConfig.module.rules ||= [];
-
-      nextConfig.module.rules.push({
-        test: /\.svg$/,
-        issuer: /\.(js|ts)x?$/,
-        use: [
-          {
-            loader: "@svgr/webpack",
-            options: {
-              dimensions: false,
-              icon: true,
-            },
-          },
-        ],
-      });
-
-      return nextConfig;
-    },
-  }),
+  (config: NextConfig) =>
+    withSentryConfig(config, {
+      org: "yapp-26th-web-2nd",
+      project: "26th-web-team-2-fe-web",
+      bundleSizeOptimizations: {
+        excludeDebugStatements: true,
+        excludeReplayShadowDom: true,
+        excludeReplayIframe: true,
+        excludeReplayWorker: true,
+      },
+      disableLogger: true,
+      silent: !process.env.CI,
+      sourcemaps: {
+        deleteSourcemapsAfterUpload: true,
+      },
+      telemetry: false,
+      tunnelRoute: "/monitoring",
+      widenClientFileUpload: true,
+    }),
+  (config: NextConfig): NextConfig =>
+    withSvgr({ ...config, svgrOptions: { dimensions: false, icon: true } }),
 ];
 
-const baseConfig: NextConfig = {
+export default plugins.reduce((acc, plugin) => plugin(acc), {
   reactStrictMode: true,
-};
-
-const mergedConfig = plugins.reduce((acc, plugin) => plugin(acc), baseConfig);
-
-export default withSentryConfig(mergedConfig, {
-  org: "yapp-26th-web-2nd",
-  project: "26th-web-team-2-fe-web",
-  bundleSizeOptimizations: {
-    excludeDebugStatements: true,
-    excludeReplayShadowDom: true,
-    excludeReplayIframe: true,
-    excludeReplayWorker: true,
-  },
-  disableLogger: true,
-  silent: !process.env.CI,
-  sourcemaps: {
-    deleteSourcemapsAfterUpload: true,
-  },
-  telemetry: false,
-  tunnelRoute: "/monitoring",
-  widenClientFileUpload: true,
-});
+} as NextConfig);

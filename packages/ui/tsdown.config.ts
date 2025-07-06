732e87b13915a2b8629f782ced8a2c314c3a740c
sudosubin <sudosubin@gmail.com>
2025-07-06 18:29:18 +0900

    [Y26W2-51] feat(ui): TaliwindCSS 용 css 파일을 별도로 붙이도록 개선
    commit-id:4e9d2cc8

--- packages/ui/tsdown.config.ts
+++ packages/ui/tsdown.config.ts
@@ -1,6 +1,9 @@
 import { exec } from "node:child_process";
+import { appendFile, readFile } from "node:fs/promises";
 import { promisify } from "node:util";
 import { defineConfig } from "tsdown";
+
+const execAsync = promisify(exec);
 
 export default defineConfig({
   entry: "./src/*/*",
@@ -11,8 +14,9 @@ export default defineConfig({
   minify: true,
   hooks: {
     "build:done": async () => {
-      const execAsync = promisify(exec);
       await execAsync("tailwindcss -i src/app.css -o dist/app.css");
+      const css = await readFile("src/app.css", "utf-8");
+      await appendFile("dist/app.css", css);
     },
   },
   external: ["react", "react-dom"],

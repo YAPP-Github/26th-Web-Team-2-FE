a7830752460042336beaf7c7fb53a5a29c0f3b4c
goeun kim <rlarhdms6828@naver.com>
2025-07-08 12:13:36 +0900

    [Y26W2-102] chore(ui): vite 실행시 보여지는 설명글 font-size 수정
    commit-id:fd329510

--- packages/ui/src/main.tsx
+++ packages/ui/src/main.tsx
@@ -4,7 +4,7 @@ import "./app.css";
 import WalkerIcon from "./assets/icons/ic_walker.svg?react";
 
 const App = () => (
-  <div className="m-0 flex h-screen flex-col items-center justify-center gap-2 bg-gray-900 p-0 font-sans text-gray-300">
+  <div className="m-0 flex h-screen flex-col items-center justify-center gap-2 bg-gray-900 p-0 font-sans text-2xl text-gray-300">
     <h1>
       👋 <strong>ssok packages/ui</strong>에 오신 걸 환영합니다! ✈️
     </h1>
@@ -16,7 +16,7 @@ const App = () => (
       재사용 가능한 컴포넌트를 개발하고 테스트하며 문서화하기 위해 이
       워크스페이스를 사용하세요.
     </p>
-    <p style={{ fontSize: "0.9rem", color: "#6b7280" }}>
+    <p style={{ fontSize: "1rem", color: "#6b7280" }}>
       <code>pnpm dev:storybook</code> 명령어로 시작해보세요!
       <WalkerIcon />
     </p>

08a0b0e60471cbc97594fe0b4ed6ce3f3092bf9f
goeun kim <rlarhdms6828@naver.com>
2025-07-18 00:59:51 +0900

    [Y26W-115] chore(ui): add-button icon aria-label 추가 및 props 재네이밍

--- packages/ui/src/icon-button/add-icon-button/index.tsx
+++ packages/ui/src/icon-button/add-icon-button/index.tsx
@@ -4,7 +4,7 @@ import IcAdd from "@/assets/icons/ic_round-plus.svg?react";
 import { cn } from "@/utils";
 import { addbutton } from "./add-icon-button.variant";
 
-type IconButtonProps = {
+type AddIconButtonProps = {
   className?: string;
   selected: boolean;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
@@ -13,15 +13,19 @@ export const AddIconButton = ({
   className,
   selected,
   ...props
-}: IconButtonProps) => {
+}: AddIconButtonProps) => {
   return (
     <button
-      aria-label="숙소 추가 버튼"
+      aria-label="추가 버튼"
       className={cn(addbutton({ selected }), className)}
       {...props}
     >
-      {!selected && <IcCheckFill width="40px" height="40px" role="img" />}
-      {selected && <IcAdd width="40px" height="40px" role="img" />}
+      {!selected && (
+        <IcCheckFill width="40px" height="40px" role="img" aria-label="확인" />
+      )}
+      {selected && (
+        <IcAdd width="40px" height="40px" role="img" aria-label="추가" />
+      )}
     </button>
   );
 };

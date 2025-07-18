deb2f0038c1a8e6175c75723db576e94e9834465
goeun kim <rlarhdms6828@naver.com>
2025-07-19 00:12:36 +0900

    [Y26W2-117] chore(ui): chip md gap 조정

--- packages/ui/src/chip/chip.variant.ts
+++ packages/ui/src/chip/chip.variant.ts
@@ -2,13 +2,13 @@ import { cva } from "class-variance-authority";
 
 export const chip = cva(
   [
-    "flex w-fit items-center gap-[0.2rem] rounded-[0.4rem] border-none px-[0.4rem] py-[0.3rem] transition-colors duration-200",
+    "flex w-fit items-center rounded-[0.4rem] border-none px-[0.4rem] py-[0.3rem] transition-colors duration-200",
   ],
   {
     variants: {
       size: {
-        xs: "text-caption2-medi11 text-neutral-50 hover:text-neutral-50 focus:text-neutral-50",
-        md: "text-body2-medi14 text-neutral-50 hover:text-neutral-60 focus:text-neutral-variant-50",
+        xs: "gap-[0.2rem] text-caption2-medi11 text-neutral-50 hover:text-neutral-50 focus:text-neutral-50",
+        md: "gap-[0.3rem] text-body2-medi14 text-neutral-50 hover:text-neutral-60 focus:text-neutral-variant-50",
       },
       icon: {
         true: "bg-neutral-98 hover:bg-neutral-95 focus:bg-neutral-variant-95",
---
+++ packages/ui/src/pin/index.tsx
@@ -0,0 +1,52 @@
+import {
+  cloneElement,
+  isValidElement,
+  type ReactElement,
+  type SVGProps,
+} from "react";
+import { cn } from "@/utils";
+import { chip } from "./pin.variant";
+
+export type ChipProps = {
+  className?: string;
+  size: "xs" | "md";
+  icon?: React.ReactNode;
+  additionalText?: string;
+  text: string;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+const iconSizeMap = {
+  xs: "12px",
+  md: "14px",
+};
+
+export const Chip = ({
+  text,
+  className,
+  size,
+  icon = false,
+  additionalText,
+  ...props
+}: ChipProps) => {
+  const iconSize = iconSizeMap[size];
+
+  const styledIcon = isValidElement(icon)
+    ? cloneElement(icon as ReactElement<SVGProps<SVGSVGElement>>, {
+        width: iconSize,
+        height: iconSize,
+        style: { width: iconSize, height: iconSize },
+      })
+    : icon;
+
+  return (
+    <button className={cn(chip({ size, icon: !!icon }), className)} {...props}>
+      {icon && (
+        <span className="flex items-center justify-center">{styledIcon}</span>
+      )}
+      <span>{text}</span>
+      {additionalText && (
+        <span className="text-neutral-40">{additionalText}</span>
+      )}
+    </button>
+  );
+};

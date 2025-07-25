60aa06f8279daf7492f0650b6e71fa6dbc1dc256
goeun kim <rlarhdms6828@naver.com>
2025-07-26 01:41:44 +0900

    [Y26W2-171] chore(web): 테스트 랜더링을 위한 mock data 생성

---
+++ apps/web/src/app/boards/[id]/lists/_mock.ts
@@ -0,0 +1,52 @@
+export const _MOCK_DATA = {
+  people: [
+    { name: "전체", id: "0" },
+    { name: "고은", id: "1" },
+    { name: "수빈", id: "2" },
+    { name: "경민", id: "3" },
+    { name: "시언", id: "4" },
+    { name: "성연", id: "5" },
+    { name: "세환", id: "6" },
+    { name: "지수", id: "7" },
+  ],
+  places: [
+    {
+      imgSrc:
+        "https://pix8.agoda.net/hotelImages/942/942521/942521_17021009050050901364.jpg?ca=6&ce=1&s=312x235&ar=16x9",
+      platform: {
+        name: "아고다",
+        href: "https://agoda.com",
+        logoSrc:
+          "https://cdn6.agoda.net/images/kite-js/logo/agoda/color-default.svg",
+      },
+      placeName: "그랜드 하얏트 제주 스페셜 럭셔리 어쩌구 저쩌구 ㅎㅎㅎ",
+      price: 450000,
+      address: "제주특별자치도 제주시 연동 123",
+      attractions: [
+        { id: "1", placeName: "한라산", distance: 15 },
+        { id: "2", placeName: "이호테우해변", distance: 8 },
+      ],
+      savedByText: "고은",
+      memoContent: "이 호텔은 조식이 맛있고 위치가 좋아요.",
+    },
+    {
+      imgSrc:
+        "https://pix8.agoda.net/hotelImages/942/942521/942521_17021009050050901364.jpg?ca=6&ce=1&s=312x235&ar=16x9",
+      platform: {
+        name: "아고다",
+        href: "https://agoda.com",
+        logoSrc:
+          "https://cdn6.agoda.net/images/kite-js/logo/agoda/color-default.svg",
+      },
+      placeName: "그랜드 하얏트 제주 스페셜 럭셔리 어쩌구 저쩌구 ㅎㅎㅎ",
+      price: 450000,
+      address: "제주특별자치도 제주시 연동 123",
+      attractions: [
+        { id: "1", placeName: "한라산", distance: 15 },
+        { id: "2", placeName: "이호테우해변", distance: 8 },
+      ],
+      savedByText: "고은",
+      memoContent: "이 호텔은 조식이 맛있고 위치가 좋아요.",
+    },
+  ],
+};

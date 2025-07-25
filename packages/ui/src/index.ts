7200d423d0466547830e44543a93df85e937c3c1
goeun kim <rlarhdms6828@naver.com>
2025-07-26 01:37:08 +0900

    [Y26W2-171] chore(ui): icon 및 common component barrel export 추가

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,6 +1,12 @@
+export { default as IcAddMemo } from "@/assets/icons/ic_add_memo.svg?react";
+export { default as IcCollapse } from "@/assets/icons/ic_collapse.svg?react";
+export { default as IcExpand } from "@/assets/icons/ic_expand.svg?react";
+export { default as IcInfo } from "@/assets/icons/ic_info.svg?react";
 export { default as IcList } from "@/assets/icons/ic_list.svg?react";
 export { default as IcTable } from "@/assets/icons/ic_table.svg?react";
+
 export { Button } from "@/components/button";
+export { Card } from "@/components/card";
 export { Chip } from "@/components/chip";
 export { Graph } from "@/components/graph";
 export { AddIconButton } from "@/components/icon-button/add-icon-button";
@@ -17,3 +23,5 @@ export { InterimTitle } from "@/components/interim-title";
 export { Pin } from "@/components/pin";
 export { TextField } from "@/components/text-field";
 export { cn } from "@/utils";
+
+export { default as TextWithIcon } from "./components/text-with-icon";

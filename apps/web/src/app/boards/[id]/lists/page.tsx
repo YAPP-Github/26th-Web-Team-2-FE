37ae99b6d446b4245f3c1f2c25cad5f7e2ce06d4
goeun kim <rlarhdms6828@naver.com>
2025-07-29 01:50:08 +0900

    [Y26W2-187] chore(web): 저장 가능한 도메인 안내 isExpanded 여부에 따른 분기처리

--- apps/web/src/app/boards/[id]/lists/page.tsx
+++ apps/web/src/app/boards/[id]/lists/page.tsx
@@ -66,22 +66,24 @@ const BoardsIdListsPage = () => {
             <p className="text-heading1-semi20 text-neutral-10">
               숙소 링크 저장하기
             </p>
-            <TextWithIcon
-              onMouseEnter={() => setIsTooltipVisible(true)}
-              onMouseLeave={() => setIsTooltipVisible(false)}
-              icon={
-                <IcInfo
-                  width="20px"
-                  height="20px"
-                  className="text-primary-70"
-                />
-              }
-              className="flex-row-reverse gap-[0.2rem]"
-            >
-              <TextWithIcon.Text className="text-caption2-regular11 text-neutral-50">
-                저장 가능한 링크
-              </TextWithIcon.Text>
-            </TextWithIcon>
+            {isInputExpanded && (
+              <TextWithIcon
+                onMouseEnter={() => setIsTooltipVisible(true)}
+                onMouseLeave={() => setIsTooltipVisible(false)}
+                icon={
+                  <IcInfo
+                    width="20px"
+                    height="20px"
+                    className="text-primary-70"
+                  />
+                }
+                className="flex-row-reverse gap-[0.2rem]"
+              >
+                <TextWithIcon.Text className="text-caption2-regular11 text-neutral-50">
+                  저장 가능한 링크
+                </TextWithIcon.Text>
+              </TextWithIcon>
+            )}
             {isTooltipVisible && (
               <BubbleInfo>
                 지금은 부킹닷컴, 아고다, 호텔스컴바인 링크만 지원 중이에요

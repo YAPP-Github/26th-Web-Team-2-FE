161d45a8303637d67a63a2014d8e5c4bf3a196a4
goeun kim <rlarhdms6828@naver.com>
2025-08-01 02:17:53 +0900

    [Y26W2-216] refactor(web): onbording tooltip 캡슐화 개선

--- apps/web/src/domains/list/components/link-input-section/atom/onboarding-bubble.tsx
+++ apps/web/src/domains/list/components/link-input-section/atom/onboarding-bubble.tsx
@@ -1,49 +1,58 @@
-import { Button, IcClose } from "@ssok/ui";
-import type React from "react";
+import { Button, cn, IcClose } from "@ssok/ui";
 
-type BaseOnboardingProps = {
-  title: string;
-  description: React.ReactNode;
-  nextBtnText: string;
-  index: number;
-  total: number;
-  onClose: () => void;
-  onNext: () => void;
+import useOnboarding from "@/domains/list/hooks/use-onBoarding";
+
+const config = {
+  init: {
+    title: "호텔, 이렇게 추가해요",
+    description: (
+      <>
+        호텔 상세 페이지 링크를 붙여넣거나 드래그해서 넣어주세요. <br />
+        현재는 부킹닷컴, 아고다, 익스피디아만 지원돼요!
+      </>
+    ),
+    nextBtnText: "다음으로",
+    index: 1,
+    total: 2,
+  },
+  saveLink: {
+    title: "호텔, 이렇게 추가해요",
+    description: (
+      <>
+        호텔 상세 페이지 링크를 붙여넣거나 드래그해서 넣어주세요. <br />
+        현재는 부킹닷컴, 아고다, 익스피디아만 지원돼요!
+      </>
+    ),
+    nextBtnText: "다음으로",
+    index: 2,
+    total: 2,
+  },
 };
 
-type OnboardingBubbleProps =
-  | (BaseOnboardingProps & {
-      prevBtnText: string;
-      onPrev: () => void;
-    })
-  | (BaseOnboardingProps & {
-      prevBtnText?: undefined;
-      onPrev?: undefined;
-    });
+const OnboardingBubble = () => {
+  const {
+    isVisible,
+    handleClose,
+    onBoardStep,
+    handleOnBoardStep,
+    handleOnPrevStep,
+  } = useOnboarding();
+  if (onBoardStep === "finish" || !isVisible) return null;
+  const { title, description, index, total, nextBtnText } = config[onBoardStep];
 
-const OnboardingBubble = ({
-  title,
-  description,
-  prevBtnText,
-  nextBtnText,
-  index,
-  total,
-  onClose,
-  onNext,
-  onPrev,
-}: OnboardingBubbleProps) => {
   return (
-    <div className="absolute bottom-[-20rem] left-[-2.5rem] inline-block">
+    <div
+      className={cn(
+        `inline-block" absolute bottom-[-20rem] left-[-2.5rem]`,
+        onBoardStep === "init" && `bottom-[-20rem] left-[-2.5rem]`,
+        onBoardStep === "saveLink" && `bottom-[-20rem] left-[-2.5rem]`,
+      )}
+    >
       <div className="relative rounded-[1.6rem] bg-neutral-variant-5 px-[2.4rem] py-[2rem] after:absolute after:top-[-12px] after:left-10 after:border-x-[8px] after:border-x-transparent after:border-t-0 after:border-b-[12px] after:border-b-neutral-variant-5 after:content-[''] ">
         <header className="flex w-full items-center justify-between">
           <h1 className="text-heading1-semi20 text-neutral-100">{title}</h1>
-          <button type="button" onClick={onClose}>
-            <IcClose
-              width={24}
-              height={24}
-              className="text-neutral-100"
-              onClick={onClose}
-            />
+          <button type="button" onClick={handleClose}>
+            <IcClose width={24} height={24} className="text-neutral-100" />
           </button>
         </header>
         <p className="pt-[0.8rem] text-start text-body1-medi16 text-neutral-90">
@@ -52,17 +61,17 @@ const OnboardingBubble = ({
         <div className="flex flex-row items-center justify-between pt-[1.6rem]">
           <p className="text-caption1-medi12 text-neutral-70">{`${index} / ${total}`}</p>
           <div className="flex flex-row gap-[0.8rem]">
-            {onPrev && (
+            {onBoardStep === "saveLink" && (
               <Button
                 variant="text"
                 size="sm"
-                onClick={onPrev}
+                onClick={handleOnPrevStep}
                 className="text-neutral-100"
               >
-                {prevBtnText}
+                이전으로
               </Button>
             )}
-            <Button variant="primary" size="sm" onClick={onNext}>
+            <Button variant="primary" size="sm" onClick={handleOnBoardStep}>
               {nextBtnText}
             </Button>
           </div>
--- apps/web/src/domains/list/hooks/use-onBoarding.tsx
+++ apps/web/src/domains/list/hooks/use-onBoarding.tsx
@@ -2,8 +2,24 @@ import { useEffect, useState } from "react";
 
 type OnBoardStep = "init" | "saveLink" | "finish";
 
+/**
+ * 온보딩 단계를 관리하는 커스텀 훅
+ *
+ * - localStorage에 저장된 `onBoardStep` 값을 기반으로 온보딩 단계를 관리합니다.
+ * - 컴포넌트에서 온보딩 상태를 UI로 제어할 수 있도록 다양한 상태와 메서드를 제공합니다.
+ *
+ * @returns {{
+ *   onBoardStep: "init" | "saveLink" | "finish";  // 현재 온보딩 단계
+ *   isVisible: boolean;                            // 온보딩 버블 표시 여부
+ *   close: () => void;                             // 온보딩 버블을 닫습니다 (isVisible을 false로 변경)
+ *   handleOnBoardStep: () => void;                 // 현재 단계에서 다음 단계로 이동합니다
+ *   handleOnPrevStep: () => void;                  // 현재 단계에서 이전 단계로 이동합니다
+ *   resetOnBoardStep: () => void;                  // 온보딩 상태를 초기화하고 1단계(init)로 되돌립니다
+ * }}
+ */
 const useOnboarding = () => {
   const [onBoardStep, setOnBoardStep] = useState<OnBoardStep>("init");
+  const [isVisible, setIsVisible] = useState(true);
 
   useEffect(() => {
     const step = (localStorage.getItem("onBoardStep") as OnBoardStep) || "init";
@@ -41,8 +57,15 @@ const useOnboarding = () => {
     setOnBoardStep("init");
   };
 
+  const handleClose = () => {
+    setIsVisible(false);
+    resetOnBoardStep();
+  };
+
   return {
     onBoardStep,
+    isVisible,
+    handleClose,
     handleOnBoardStep,
     handleOnPrevStep,
     resetOnBoardStep,

2297db66472e0d16388b1c328c3239e6dfea488d
goeun kim <rlarhdms6828@naver.com>
2025-08-01 02:46:55 +0900

    [Y26W2-216] feat(web): step에 따른 위치 및 문구 조정

--- apps/web/src/domains/list/components/link-input-section/atom/onboarding-bubble.tsx
+++ apps/web/src/domains/list/components/link-input-section/atom/onboarding-bubble.tsx
@@ -16,18 +16,17 @@ const config = {
     total: 2,
   },
   saveLink: {
-    title: "호텔, 이렇게 추가해요",
+    title: "이제 저장해볼 차례!",
     description: (
       <>
-        호텔 상세 페이지 링크를 붙여넣거나 드래그해서 넣어주세요. <br />
-        현재는 부킹닷컴, 아고다, 익스피디아만 지원돼요!
+        저장하기 버튼을 누르면 아래 리스트에서 바로 <br /> 확인할 수 있어요!
       </>
     ),
-    nextBtnText: "다음으로",
+    nextBtnText: "시작하기",
     index: 2,
     total: 2,
   },
-};
+} as const;
 
 const OnboardingBubble = () => {
   const {
@@ -43,9 +42,9 @@ const OnboardingBubble = () => {
   return (
     <div
       className={cn(
-        `inline-block" absolute bottom-[-20rem] left-[-2.5rem]`,
-        onBoardStep === "init" && `bottom-[-20rem] left-[-2.5rem]`,
-        onBoardStep === "saveLink" && `bottom-[-20rem] left-[-2.5rem]`,
+        "absolute z-10 inline-block",
+        onBoardStep === "init" && "bottom-[-12rem] left-[0rem]",
+        onBoardStep === "saveLink" && "bottom-[-20rem] left-[84%] w-max",
       )}
     >
       <div className="relative rounded-[1.6rem] bg-neutral-variant-5 px-[2.4rem] py-[2rem] after:absolute after:top-[-12px] after:left-10 after:border-x-[8px] after:border-x-transparent after:border-t-0 after:border-b-[12px] after:border-b-neutral-variant-5 after:content-[''] ">

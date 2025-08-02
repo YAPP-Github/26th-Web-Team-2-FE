c8d246a499b60e447669ddd82c91fe0b7e63ab70
goeun kim <rlarhdms6828@naver.com>
2025-08-02 12:34:23 +0900

    [Y26W2-220] chore(web): 누락된 context provider 추가 및 헤더 텍스트 수정

--- apps/web/src/app/boards/[id]/layout.tsx
+++ apps/web/src/app/boards/[id]/layout.tsx
@@ -1,16 +1,19 @@
 import type { PropsWithChildren } from "react";
+import { AccommodationProvider } from "@/domains/list/contexts/accomodation-context";
 import MapComponent from "@/shared/components/map-component";
 import SideNavigation from "@/shared/components/side-navigation";
 
 const BoardsIdLayout = async ({ children }: PropsWithChildren) => {
   return (
-    <div className="flex h-screen w-full overflow-hidden">
-      <SideNavigation />
-      <div className="relative flex min-w-0 flex-1">
-        {children}
-        <MapComponent className="transition-all duration-500 ease-in-out" />
+    <AccommodationProvider>
+      <div className="flex h-screen w-full overflow-hidden">
+        <SideNavigation />
+        <div className="relative flex min-w-0 flex-1">
+          {children}
+          <MapComponent className="transition-all duration-500 ease-in-out" />
+        </div>
       </div>
-    </div>
+    </AccommodationProvider>
   );
 };
 

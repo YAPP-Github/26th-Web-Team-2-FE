5417af79d9edb8e03f819c98a0afe974191f39e5
goeun kim <rlarhdms6828@naver.com>
2025-08-06 15:33:59 +0900

    [Y26W2-231] feat(web): mock 데이터로 board header 랜더링

--- apps/web/src/domains/list/components/header-section/index.tsx
+++ apps/web/src/domains/list/components/header-section/index.tsx
@@ -1,22 +1,23 @@
 import { Button, IcArrowLeft, IcPerson } from "@ssok/ui";
+import { formatDate } from "@/shared/utils/date";
+import { useBoardData } from "../../hooks/use-board-data";
 
 const HeaderSection = () => {
+  const { boardName, place, startDate, endDate, members } = useBoardData();
   return (
     <header className="flex w-full items-center justify-between p-2 pr-8 pl-2">
       {/* 헤더 좌측  */}
       <div className="flex items-center gap-[1.2rem]">
         <div className="flex items-center gap-[0.8rem]">
           <IcArrowLeft width={24} height={24} className="text-neutral-40" />
-          <span className="text-body1-medi16 text-neutral-25">
-            여름맞이 휴가가자
-          </span>
+          <span className="text-body1-medi16 text-neutral-25">{boardName}</span>
         </div>
         <div className="flex items-center gap-[0.8rem]">
-          <p className="text-body2-semi14 text-neutral-60">방콕</p>
+          <p className="text-body2-semi14 text-neutral-60">{place}</p>
           <div className="flex gap-[0.4rem] rounded-[0.4rem] bg-neutral-95 px-[0.8rem] py-[0.3rem] text-caption1-medi12 text-neutral-60">
-            <p>25.08.04</p>
+            <p>{formatDate(new Date(startDate), { shortYear: true })}</p>
             <p>~</p>
-            <p>25.08.07</p>
+            <p>{formatDate(new Date(endDate), { shortYear: true })}</p>
           </div>
         </div>
       </div>
@@ -24,7 +25,7 @@ const HeaderSection = () => {
       <div className="flex gap-[0.8rem]">
         <span className="flex items-center">
           <IcPerson width={16} height={16} className="text-neutral-40" />
-          <p className="text-body2-semi14 text-neutral-40">1</p>
+          <p className="text-body2-semi14 text-neutral-40">{members.length}</p>
         </span>
         <Button size="xxs" variant="black">
           초대하기

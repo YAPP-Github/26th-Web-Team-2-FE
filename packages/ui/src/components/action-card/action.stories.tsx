57b7948edcacb80eda9b7e34635e81e75689c67d
goeun kim <rlarhdms6828@naver.com>
2025-08-07 01:03:54 +0900

    [Y26W2-242] feat(ui): ActionCard 컴포넌트 제작 및 스토리북 설정

---
+++ packages/ui/src/components/action-card/action.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { ActionCard } from ".";
+
+const meta: Meta<typeof ActionCard> = {
+  title: "Components/ActionCard",
+  component: ActionCard,
+  tags: ["autodocs"],
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          width: "384px",
+          height: "242px",
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ActionCard>;
+
+export const Default: Story = {
+  args: {
+    onClick: () => {},
+  },
+};
---
+++ packages/ui/src/components/action-card/index.tsx
@@ -0,0 +1,26 @@
+import { cn, IcAdd } from "dist";
+
+type ActionCardProps = {
+  onClick: () => void;
+};
+
+export const ActionCard = ({ onClick, ...props }: ActionCardProps) => {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={cn(
+        `flex min-h-[24.2rem] min-w-[38.4rem] flex-col items-center justify-center gap-[0.8rem] rounded-[1.6rem]`,
+        `border border-dashed`,
+        `border-neutral-80 bg-neutral-98`,
+        `hover:border-neutral-70 hover:bg-neutral-95`,
+        `focus:border-neutral-70 focus:bg-neutral-95`,
+        `active:border-neutral-variant-70 active:bg-neutral-variant-95`,
+      )}
+      {...props}
+    >
+      <IcAdd width={32} height={32} className="text-neutral-70" />
+      <p className="text-heading1-semi20 text-neutral-70">새 여행 만들기</p>
+    </button>
+  );
+};

febfaac763b9a716f805e677736f952eeb2dd131
goeun kim <rlarhdms6828@naver.com>
2025-08-08 19:38:04 +0900

    [Y26W2-231] refactor(web): 날짜 형식 props로 전달 (ex.YYMMDD YYYYMMDD)

--- apps/web/src/shared/utils/date.ts
+++ apps/web/src/shared/utils/date.ts
@@ -1,13 +1,13 @@
 type FormatDateOptions = {
-  shortYear?: boolean; // true이면 "YY.MM.DD", false이면 "YYYY.MM.DD"
+  format?: "YY.MM.DD" | "YYYY.MM.DD";
 };
 
 export const formatDate = (
   date: Date,
-  { shortYear = false }: FormatDateOptions = {},
+  { format = "YYYY.MM.DD" }: FormatDateOptions = {},
 ): string => {
   const yearFull = String(date.getFullYear());
-  const year = shortYear ? yearFull.slice(2) : yearFull;
+  const year = format === "YY.MM.DD" ? yearFull.slice(2) : yearFull;
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const day = String(date.getDate()).padStart(2, "0");
   return `${year}.${month}.${day}`;

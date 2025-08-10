29e9ce7201af6f65e39f2a34ee1327809f32277b
Subin Kim <sudosubin@gmail.com>
2025-08-10 14:48:44 +0900

    [Y26W2-126] feat(taliwind-config): scrollbar 관련 util class 추가 (#104)
    commit-id:fdfbf92d

--- packages/tailwind-config/src/plugin.ts
+++ packages/tailwind-config/src/plugin.ts
@@ -1,6 +1,32 @@
 import plugin_ from "tailwindcss/plugin";
 import { theme } from "@/theme";
 
-export const plugin = plugin_(() => {}, {
-  theme: { extend: theme },
-});
+export const plugin = plugin_(
+  ({ addUtilities }) => {
+    addUtilities({
+      ".scrollbar-hide": {
+        /* IE and Edge */
+        "-ms-overflow-style": "none",
+        /* Firefox */
+        "scrollbar-width": "none",
+        /* Safari and Chrome */
+        "&::-webkit-scrollbar": {
+          display: "none",
+        },
+      },
+      ".scrollbar-default": {
+        /* IE and Edge */
+        "-ms-overflow-style": "auto",
+        /* Firefox */
+        "scrollbar-width": "auto",
+        /* Safari and Chrome */
+        "&::-webkit-scrollbar": {
+          display: "block",
+        },
+      },
+    });
+  },
+  {
+    theme: { extend: theme },
+  },
+);

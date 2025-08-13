05cc40718c57e8e6cc4b918be37c1b1723d56625
Subin Kim <sudosubin@gmail.com>
2025-08-14 01:13:12 +0900

    [Y26W2-301] fix(ui): SSR 과정에서 발생하는 document 오류 수정 (#119)
    * [Y26W2-301] fix(ui): SSR 과정에서 발생하는 document 오류 수정
    
    * [Y26W2-301] fix(root): 그 외 vite 패키지들도 수정

--- packages/tailwind-config/vite.config.ts
+++ packages/tailwind-config/vite.config.ts
@@ -1,20 +1,19 @@
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
+import { externalizeDeps } from "vite-plugin-externalize-deps";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     dts({ include: ["src"], entryRoot: "src", rollupTypes: true }),
     tsConfigPaths(),
+    externalizeDeps(),
   ],
   build: {
     lib: {
       entry: "src/index.ts",
       fileName: "index",
       formats: ["es"],
-    },
-    rollupOptions: {
-      external: ["tailwindcss", "tailwindcss/plugin"],
     },
   },
 });

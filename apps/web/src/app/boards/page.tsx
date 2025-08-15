26ccb3acfd010fdeb97acecbb747993bc741cfaf
sudosubin <sudosubin@gmail.com>
2025-08-16 01:32:03 +0900

    [Y26W2-319] refactor(web): boards infinite query 최적화

--- apps/web/src/app/boards/page.tsx
+++ apps/web/src/app/boards/page.tsx
@@ -1,67 +1,34 @@
-"use client";
-import { ActionCard, TravelBoard } from "@ssok/ui";
-import useTripBoardList from "@/domains/dashboard/hooks/use-trip-board-list";
-import Header from "@/shared/components/header";
-import { useSession } from "@/shared/hooks/use-session";
+import { prefetchGetTripBoardsInfiniteQuery } from "@ssok/api";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { redirect } from "next/navigation";
+import { auth } from "@/domains/auth";
+import DashboardView from "@/domains/dashboard/views/dashboard-view";
+import getQueryClient from "@/shared/configs/tanstack-query/get-query-client";
 
-const DashBoardPage = () => {
-  const { accessToken } = useSession({ required: true });
-  const { data: tripBoards } = useTripBoardList(
-    { size: 10 },
+const DashboardPage = async () => {
+  const session = await auth.getSession({ refresh: false });
+  if (!session) {
+    redirect("/api/auth/login?to=/");
+  }
+
+  const queryClient = getQueryClient();
+  await prefetchGetTripBoardsInfiniteQuery(
+    queryClient,
+    { page: 0, size: 10 },
     {
-      accessToken: accessToken || "",
-      enabled: !!accessToken,
+      request: {
+        headers: {
+          Authorization: `Bearer ${session.tokenSet.accessToken}`,
+        },
+      },
     },
   );
 
-  const allTripBoards =
-    tripBoards?.pages?.flatMap((page) => page?.result?.tripBoards ?? []) ?? [];
-
   return (
-    <main>
-      {/* 페이지 헤더 */}
-      <Header />
-      {/* 제목 + 여행 보드 목록 */}
-      <section className="px-[10.4rem] pt-[7.2rem]">
-        <h1 className="mb-[3.6rem] text-neutral-10 text-title1-semi36">
-          나의 여행
-        </h1>
-        <ul className="grid grid-cols-3 gap-[4rem]">
-          <li>
-            <ActionCard onClick={() => alert("여행생성")} className="w-full" />
-          </li>
-          {allTripBoards.map((tripBoard) => (
-            <li key={tripBoard.tripBoardId}>
-              <TravelBoard
-                data={{
-                  boardId: tripBoard.tripBoardId!,
-                  boardName: tripBoard.boardName!,
-                  destination: tripBoard.destination!,
-                  startDate: tripBoard.startDate?.toString()!,
-                  endDate: tripBoard.endDate?.toString()!,
-                  participantCount: tripBoard.participantCount!,
-                  participants:
-                    tripBoard.participants?.map((p) => ({
-                      userId: p.userId ?? 0,
-                      profileImageUrl: p.profileImageUrl ?? "",
-                      nickname: p.nickname ?? "",
-                      role: p.role ?? "MEMBER",
-                    })) ?? [],
-                  accommodationCount: tripBoard.accommodationCount!,
-                }}
-                // TODOT: 핸들러 함수 api 부착
-                onDeleteClick={() => alert("여행 삭제")}
-                onEditClick={() => alert("여행 수정")}
-                onExitClick={() => alert("여행 나가기")}
-                onInviteClick={() => alert("여행 초대")}
-                className="w-full"
-              />
-            </li>
-          ))}
-        </ul>
-      </section>
-    </main>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <DashboardView />
+    </HydrationBoundary>
   );
 };
 
-export default DashBoardPage;
+export default DashboardPage;
---
+++ apps/web/src/domains/dashboard/views/dashboard-view.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import { useGetTripBoardsInfinite } from "@ssok/api";
+import { ActionCard, TravelBoard } from "@ssok/ui";
+import { useEffect } from "react";
+import Header from "@/shared/components/header";
+import { useSession } from "@/shared/hooks/use-session";
+
+const DashboardView = () => {
+  const { accessToken } = useSession({ required: true });
+
+  const {
+    data: { pages } = {},
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+  } = useGetTripBoardsInfinite(
+    { page: 0, size: 10 },
+    {
+      query: {
+        enabled: !!accessToken,
+        getNextPageParam: (lastPage, allPages) => {
+          return lastPage.data.result?.hasNext ? allPages.length : undefined;
+        },
+      },
+      request: { headers: { Authorization: `Bearer ${accessToken}` } },
+    },
+  );
+
+  const allTripBoards =
+    pages?.flatMap((page) => page?.data.result?.tripBoards ?? []) ?? [];
+
+  useEffect(() => {
+    if (accessToken && hasNextPage && !isFetching) {
+      fetchNextPage();
+    }
+  }, [accessToken, hasNextPage, isFetching, fetchNextPage]);
+
+  return (
+    <main>
+      {/* 페이지 헤더 */}
+      <Header />
+      {/* 제목 + 여행 보드 목록 */}
+      <section className="px-[10.4rem] pt-[7.2rem]">
+        <h1 className="mb-[3.6rem] text-neutral-10 text-title1-semi36">
+          나의 여행
+        </h1>
+        <ul className="grid grid-cols-3 gap-[4rem]">
+          <li>
+            <ActionCard onClick={() => alert("여행생성")} className="w-full" />
+          </li>
+          {allTripBoards.map((tripBoard) => (
+            <li key={tripBoard.tripBoardId}>
+              <TravelBoard
+                data={{
+                  boardId: tripBoard.tripBoardId!,
+                  boardName: tripBoard.boardName!,
+                  destination: tripBoard.destination!,
+                  startDate: tripBoard.startDate?.toString()!,
+                  endDate: tripBoard.endDate?.toString()!,
+                  participantCount: tripBoard.participantCount!,
+                  participants:
+                    tripBoard.participants?.map((p) => ({
+                      userId: p.userId ?? 0,
+                      profileImageUrl: p.profileImageUrl ?? "",
+                      nickname: p.nickname ?? "",
+                      role: p.role ?? "MEMBER",
+                    })) ?? [],
+                  accommodationCount: tripBoard.accommodationCount!,
+                }}
+                // TODOT: 핸들러 함수 api 부착
+                onDeleteClick={() => alert("여행 삭제")}
+                onEditClick={() => alert("여행 수정")}
+                onExitClick={() => alert("여행 나가기")}
+                onInviteClick={() => alert("여행 초대")}
+                className="w-full"
+              />
+            </li>
+          ))}
+        </ul>
+        {isFetching && (
+          <div className="mt-[2rem] text-center">
+            <p className="text-body2-medi14 text-neutral-60">
+              데이터를 불러오는 중...
+            </p>
+          </div>
+        )}
+      </section>
+    </main>
+  );
+};
+
+export default DashboardView;

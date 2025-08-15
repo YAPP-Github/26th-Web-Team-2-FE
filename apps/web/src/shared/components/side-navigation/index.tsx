30955b8dc7bea0c2f7f61c5a421f96d2951d8f9f
goeun kim <rlarhdms6828@naver.com>
2025-08-15 21:13:43 +0900

    [Y26W2-265] feat(web): 하단 토글 메뉴 이벤트 핸들러 연결

--- apps/web/src/shared/components/side-navigation/index.tsx
+++ apps/web/src/shared/components/side-navigation/index.tsx
@@ -1,12 +1,36 @@
 "use client";
 
+import { useGetUserInfo } from "@ssok/api";
 import { AvatarProfile, cn } from "@ssok/ui";
 import Link from "next/link";
+import { useRef, useState } from "react";
+import { useOutsideClick } from "@/domains/list/hooks/use-outside-click";
 import LogoSymbol from "@/shared/assets/logo-symbol.svg";
 import useNavigationMenus from "@/shared/components/side-navigation/use-navigation-menus";
+import { useSession } from "@/shared/hooks/use-session";
 
 const SideNavigation = () => {
   const menus = useNavigationMenus();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { accessToken } = useSession({ required: true });
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  const { data: userInfo } = useGetUserInfo({
+    query: {
+      enabled: !!accessToken,
+    },
+    request: { headers: { Authorization: `Bearer ${accessToken}` } },
+  });
+
+  const onOpenMenu = () => setIsMenuOpen(true);
+  const onCloseMenu = () => setIsMenuOpen(false);
+  const onToggleMenu = () => setIsMenuOpen((prev) => !prev);
+
+  useOutsideClick(menuRef, () => {
+    if (isMenuOpen) {
+      onCloseMenu();
+    }
+  });
 
   return (
     <nav
@@ -54,9 +78,58 @@ const SideNavigation = () => {
         </div>
       </section>
       {/* 사이드바_하단_프로필 */}
-      <button type="button">
-        <AvatarProfile size={48} />
-      </button>
+      <div className="relative" ref={menuRef}>
+        <button
+          type="button"
+          onClick={onToggleMenu}
+          onMouseEnter={onOpenMenu}
+          className="flex items-center justify-center"
+        >
+          <AvatarProfile
+            size={48}
+            imgUrl={userInfo?.data.result?.profileImageUrl}
+          />
+        </button>
+        {/* 사이드바_하단_토글메뉴 */}
+        {isMenuOpen && (
+          <section
+            className={cn(
+              "absolute bottom-0 left-[150%] z-[1] flex w-[30.2rem] flex-col gap-[2rem] rounded-[1.6rem] border border-neutral-90",
+              "bg-neutral-100 px-[0.8rem] py-[2.4rem] shadow-[4px_4px_8px_0_rgba(0,0,0,0.15)]",
+            )}
+          >
+            <header className="flex flex-col gap-[0.4rem] px-[1.6rem]">
+              <p className="text-caption1-medi12 text-neutral-10">
+                현재 로그인한 계정
+              </p>
+              <p className="text-body1-semi16 text-neutral-10">
+                ssokssok@gmail.com
+              </p>
+            </header>
+            {/* 회원관리 버튼 */}
+            <ul className="flex flex-col gap-[0.8rem]">
+              <li
+                key="로그아웃"
+                className={cn(
+                  "rounded-[1.2rem] px-[1.6rem] py-[0.8rem] text-body2-semi14 text-neutral-60",
+                  "hover:bg-neutral-95 focus:bg-neutral-90",
+                )}
+              >
+                <button type="button">로그아웃</button>
+              </li>
+              <li
+                key="회원탈퇴"
+                className={cn(
+                  "rounded-[1.2rem] px-[1.6rem] py-[0.8rem] text-body2-semi14 text-neutral-60",
+                  "hover:bg-neutral-95 focus:bg-neutral-90",
+                )}
+              >
+                <button type="button">회원탈퇴</button>
+              </li>
+            </ul>
+          </section>
+        )}
+      </div>
     </nav>
   );
 };

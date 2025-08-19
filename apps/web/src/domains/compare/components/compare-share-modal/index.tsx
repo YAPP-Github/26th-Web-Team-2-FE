f55425efffed7a434f4cdc8543b1a2ad62986449
GoeunKim <rlarhdms6828@naver.com>
2025-08-19 13:01:57 +0900

    [Y26W2-340] feat(web): 비교표 공유하기 구현  (#157)
    * [Y26W2-340] feat(web): 비교표 세부페이지 내부 공유하기 기능 구현
    
    * [Y26W2-340] feat(web): 비교표 목록 페이지 내부 공유하기 구현
    
    * [Y26W2-340] fix(web): 쿼리 스트링 url 파싱

---
+++ apps/web/src/domains/compare/components/compare-share-modal/index.tsx
@@ -0,0 +1,45 @@
+import { IcLink, Popup, useToast } from "@ssok/ui";
+
+type CompareShareModalProps = {
+  active: boolean;
+  deactivate: () => void;
+  shareCode?: string;
+};
+
+const CompareShareModal = ({
+  active,
+  deactivate,
+  shareCode,
+}: CompareShareModalProps) => {
+  const { toast } = useToast();
+  const onClick = async () => {
+    if (!shareCode) return;
+    try {
+      const url = new URL(window.location.href);
+      url.searchParams.set("shareCode", shareCode);
+      await navigator.clipboard.writeText(url.toString());
+      toast.success("링크가 복사되었어요");
+    } catch (error) {
+      console.error("링크 복사에 실패했어요", error);
+      toast.success("링크 복사에 실패했어요");
+    }
+  };
+  return (
+    <Popup
+      title="비교표 내용을 친구에게 공유해보세요"
+      active={active}
+      onClose={deactivate}
+      className="min-w-[55.9rem]"
+    >
+      <ul className="flex items-center justify-center pt-[2rem] pb-[3.6rem]">
+        <li className="flex w-fit rounded-full border-[0.2rem] border-neutral-95 p-[0.5rem]">
+          <button type="button" onClick={onClick}>
+            <IcLink width={41} height={41} className="text-neutral-50" />
+          </button>
+        </li>
+      </ul>
+    </Popup>
+  );
+};
+
+export default CompareShareModal;

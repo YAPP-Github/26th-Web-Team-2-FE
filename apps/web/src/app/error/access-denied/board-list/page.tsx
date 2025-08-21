b1fdc35819c996f1b598ae0878643ce187d5ee20
Subin Kim <sudosubin@gmail.com>
2025-08-21 22:59:34 +0900

    [Y26W2-355] fix(web): Suspense가 모든 페이지를 대상으로도 감싸지는 문제 수정 (#172)
    commit-id:3e815526

--- apps/web/src/app/error/access-denied/board-list/page.tsx
+++ apps/web/src/app/error/access-denied/board-list/page.tsx
@@ -1,20 +1,11 @@
-"use client";
-
-import { useRouter, useSearchParams } from "next/navigation";
 import AccessDeniedView from "@/shared/views/access-denied-view";
 
-const ErrorAccessDeniedBoardListPage = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-
-  const handleRetry = () => {
-    const to = searchParams.get("to");
-    if (to) {
-      router.push(to);
-    } else {
-      router.push("/boards");
-    }
-  };
+const ErrorAccessDeniedBoardListPage = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ to?: string }>;
+}) => {
+  const { to } = await searchParams;
 
   return (
     <AccessDeniedView
@@ -22,7 +13,7 @@ const ErrorAccessDeniedBoardListPage = () => {
       description="이 보드를 볼 수 있는 권한이 없어요."
       errorCode="HTTP ERROR 403"
       buttonText="다시 시도"
-      onRetry={handleRetry}
+      to={to}
     />
   );
 };

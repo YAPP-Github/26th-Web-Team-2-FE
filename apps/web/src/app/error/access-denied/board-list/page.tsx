07524e16cb0b3c2ee3811dcfdb01a86b290528f9
Subin Kim <sudosubin@gmail.com>
2025-08-22 22:58:06 +0900

    [Y26W2-364] fix(web): 보드 공유 URL 및 오류 페이지 URL 수정 (#181)

--- apps/web/src/app/error/access-denied/board-list/page.tsx
+++ apps/web/src/app/error/access-denied/board-list/page.tsx
@@ -9,7 +9,7 @@ const ErrorAccessDeniedBoardListPage = async ({
 
   return (
     <AccessDeniedView
-      title="www.ssok.info에 대한 액세스가 거부됨"
+      title="ssok.info에 대한 액세스가 거부됨"
       description="이 보드를 볼 수 있는 권한이 없어요."
       errorCode="HTTP ERROR 403"
       buttonText="다시 시도"
